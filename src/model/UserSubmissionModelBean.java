bc1291779112ee471979f7112c9977147c30163a
Amoki <hugo.duroux@gmail.com>
2016-05-25 09:35:29 +0200

    user signup

--- src/model/UserSubmissionModelBean.java
+++ src/model/UserSubmissionModelBean.java
@@ -1,14 +1,37 @@
 package model;
 
+import dao.fabric.DaoFabric;
+import dao.instance.UserDao;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.util.Map;
 
 @ManagedBean
 @RequestScoped
 //Durée de vue uniquement lors d'une requète
 // même propriétés que UserModelBean mais portée différente
 public class UserSubmissionModelBean extends UserModelBean{
+    private UserDao userDao;
     public UserSubmissionModelBean() {
-
+        this.userDao= DaoFabric.getInstance().createUserDao();
+    }
+    public String checkUser(SignupBean signupBean){
+        UserModelBean user = null; //this.userDao.checkUser(signupBean.getLogin(), signupBean.getPwd());
+        System.out.println(user);
+        if( user!=null){
+            //récupère l'espace de mémoire de JSF
+            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+            Map<String, Object> sessionMap = externalContext.getSessionMap();
+            //place l'utilisateur dans l'espace de mémoire de JSF
+            sessionMap.put("loggedUser", user);
+            //redirect the current page
+            return "userDisplay.xhtml";
+        } else{
+            //redirect the current page
+            return "signup.xhtml";
+        }
     }
 }
---
+++ src/validators/NameValidator.java
@@ -0,0 +1,42 @@
+package validators;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("validators.NameValidator")
+public class NameValidator implements Validator{
+
+    private static final String NAME_PATTERN = "^[A-Za-z0-9-]$";
+
+    private Pattern pattern;
+    private Matcher matcher;
+
+    public NameValidator(){
+        pattern = Pattern.compile(NAME_PATTERN);
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component,
+                         Object value) throws ValidatorException {
+
+        matcher = pattern.matcher(value.toString());
+        if(!matcher.matches()){
+
+            FacesMessage msg =
+                    new FacesMessage("Name validation failed.",
+                            "Invalid name format.");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+
+        }
+
+    }
+}
+
